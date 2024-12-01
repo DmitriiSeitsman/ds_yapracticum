c467c8ede6fc8d2b3f9d025da40543e4465d48b8
Dmitrii Seitsman <seytsman@gmail.com>
2024-12-01 19:34:24 +0300

    Базовая задача выполнена, попробую решить усложненную

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -6,11 +6,17 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+var counterDigit: Int = 0
+    @IBOutlet var Button: UIButton!
+    @IBAction func counterPush(_ sender: Any) {
+        counterDigit += 1
+        counter.text = "\(counterDigit)"
+    }
+    @IBOutlet var counter: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        counter.text = "\(counterDigit)"
         // Do any additional setup after loading the view.
     }
 
