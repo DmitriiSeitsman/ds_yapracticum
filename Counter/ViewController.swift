9539d5021fba4f7d16f30e1bdbb38acf16a31d1d
Dmitrii Seitsman <seytsman@gmail.com>
2024-12-05 11:33:43 +0300

    учтены рекомендации, полученные после ревью, код исправлен

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,53 +7,54 @@
 
 import UIKit
 import Foundation
-//var dateAndTime = Date().formatted(.dateTime)
-var counterDigit: Int = 0
-var arrayOfchanges: [String] = []
-var digit: String = "0"
-class ViewController: UIViewController {
-    @IBOutlet var counter: UILabel!
-    @IBOutlet var plusButton: UIButton!
-    @IBOutlet var minusButton: UIButton!
-    @IBOutlet var textField: UITextView!
-    //функция обновления текстового поля
-    func changesHistory() {
-        arrayOfchanges.append("[\(Date().formatted(.dateTime))]: значение изменено на \(digit)")
-        textField.text = arrayOfchanges.joined(separator: "\n")
+
+final class ViewController: UIViewController {
+    private var counterValue: Int = 0
+    private var changeHistory: [String] = []
+    private var lastChange: String = "0"
+    @IBOutlet private var counter: UILabel!
+    @IBOutlet private var plusButton: UIButton!
+    @IBOutlet private var minusButton: UIButton!
+    @IBOutlet private var textField: UITextView!
+   
+   override func viewDidLoad() {
+        super.viewDidLoad()
+        counter.text = "\(counterValue)"
+        textField.text = "История изменений:"
+    }
+    //функция обновления истории изменений счетчика
+    private func changesHistory() {
+        changeHistory.append("[\(Date().formatted(.dateTime))]: значение изменено на \(lastChange)")
+        textField.text = changeHistory.joined(separator: "\n")
     }
     //Нажатие кнопки плюс
-    @IBAction func counterPush(_ sender: Any) {
-        digit = "+1"
-        counterDigit += 1
-        counter.text = "\(counterDigit)"
+    @IBAction private func counterPush(_ sender: Any) {
+        lastChange = "+1"
+        counterValue += 1
+        counter.text = "\(counterValue)"
         changesHistory()
     }
     //Нажатие кнопки минус
-    @IBAction func minusPress(_ sender: Any) {
-        digit = "-1"
-        if counter.text == "0" {
-            arrayOfchanges.append("[\(Date().formatted(.dateTime))]: попытка уменьшить значение счетчика ниже 0")
-            textField.text = arrayOfchanges.joined(separator: "\n")
+    @IBAction private func minusPress(_ sender: Any) {
+        lastChange = "-1"
+        if counterValue == 0 {
+            changeHistory.append("[\(Date().formatted(.dateTime))]: попытка уменьшить значение счетчика ниже 0")
+            textField.text = changeHistory.joined(separator: "\n")
             let alert = UIAlertController(title: "Ошибка", message: "0 - минимальное значение счетчика", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             present(alert, animated: true, completion: nil)
         } else {
-            counterDigit -= 1
-            counter.text = "\(counterDigit)"
+            counterValue -= 1
+            counter.text = "\(counterValue)"
             changesHistory()
         }
     }
 //Нажатие кнопки сброс
-    @IBAction func resetPress(_ sender: Any) {
-        arrayOfchanges.append("[\(Date().formatted(.dateTime))]: значение сброшено на 0")
-        textField.text = arrayOfchanges.joined(separator: "\n")
-        counterDigit = 0
-        counter.text = "\(counterDigit)"
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        counter.text = "\(counterDigit)"
-        textField.text = "История изменений:"
+    @IBAction private func resetPress(_ sender: Any) {
+        changeHistory.append("[\(Date().formatted(.dateTime))]: значение сброшено на 0")
+        textField.text = changeHistory.joined(separator: "\n")
+        counterValue = 0
+        counter.text = "\(counterValue)"
     }
 }
 
