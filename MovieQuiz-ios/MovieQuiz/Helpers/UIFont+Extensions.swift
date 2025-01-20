851d45cd0532d83d1337ff4e06869d958cb3acd0
Dmitrii Seitsman <seytsman@gmail.com>
2025-01-20 16:52:59 +0300

    убран ненужный комментарий. финальная версия

--- MovieQuiz-ios/MovieQuiz/Helpers/UIFont+Extensions.swift
+++ MovieQuiz-ios/MovieQuiz/Helpers/UIFont+Extensions.swift
@@ -1,7 +1,5 @@
 import UIKit
-//extension UIFont {
-//    static var ypDisplayMedium: UIFont { UIFont(name: "YS Display-Medium", size: 20) ?? UIFont.systemFont(ofSize: 10)}
-//}
+
 extension UIFont {
     private static let ysDisplayMediumFontName: String = "YSDisplay-Medium"
     static let ysDisplayMedium20 = UIFont(name: ysDisplayMediumFontName, size: 20)
